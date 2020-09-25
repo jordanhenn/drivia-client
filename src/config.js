e4f8ed4d217bc5f79bd389e209e9c3d5fc38518b
Jordan Henn <jordanwalkerhenn@gmail.com>
2020-09-24 23:26:35 -0700

    built mvp

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'https://peaceful-basin-11084.herokuapp.com/api',
+    API_ENDPOINT: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api',
     TOKEN_KEY: 'drivia-client-auth-token',
   }
   
